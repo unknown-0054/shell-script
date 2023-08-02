681063167cc8fa4ae214bb4885a83ff6b9433641
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2023-08-03 00:28:38 +0800

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -44,29 +44,70 @@ systemctl daemon-reload
 
 # 内核参数调整
 cat >'/etc/sysctl.d/99-sysctl.conf' <<EOF
-# 开启 bbr，内核版本 >= 4.9
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr
-
-# 增大可用的客户端端口范围
-net.ipv4.ip_local_port_range = 1024 65535
-
-# 尽量多复用连接
-net.ipv4.tcp_timestamps = 1
+kernel.msgmnb = 65536
+kernel.msgmax = 65536
+kernel.shmmax = 68719476736
+kernel.shmall = 4294967296
+vm.swappiness = 10
+vm.dirty_background_bytes = 52428800
+vm.dirty_bytes = 52428800
+vm.dirty_ratio = 0
+vm.dirty_background_ratio = 0
+net.core.rps_sock_flow_entries = 65536
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 131072
+net.ipv4.conf.all.route_localnet = 1
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.conf.default.forwarding = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.default.rp_filter = 0
+net.ipv4.conf.all.rp_filter = 0
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_retries1 = 3
+net.ipv4.tcp_retries2 = 5
+net.ipv4.tcp_orphan_retries = 1
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_synack_retries = 3
 net.ipv4.tcp_tw_reuse = 1
-
-# 增大 tcp 缓冲队列，若仍然不够可适当再增加前两项
-net.ipv4.tcp_max_syn_backlog = 4096
-net.core.somaxconn = 4096
-net.ipv4.tcp_abort_on_overflow = 1
-
-# 服务器网络非常稳定设置 0，提升吞吐量，服务器网络不够稳定则设置 1
-net.ipv4.tcp_slow_start_after_idle = 1
-
-# 尽量少用 swap，多用物理内存；设置 0 表示不使用 swap，设置 100 表示优先使用 swap
-vm.swappiness = 100
-
-fs.file-max = 6553560
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_max_tw_buckets = 262144
+net.ipv4.tcp_max_syn_backlog = 4194304
+net.core.netdev_max_backlog = 4194304
+net.core.somaxconn = 65536
+net.ipv4.tcp_notsent_lowat = 16384
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.tcp_keepalive_probes = 3
+net.ipv4.tcp_keepalive_intvl = 60
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_autocorking = 0
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_frto = 0
+net.ipv4.tcp_mtu_probing = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
+net.core.rmem_max = 33554432
+net.core.wmem_max = 33554432
+net.ipv4.tcp_rmem = 16384 131072 67108864
+net.ipv4.tcp_wmem = 4096 16384 33554432
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+net.ipv4.tcp_mem = 262144 1048576 4194304
+net.ipv4.udp_mem = 262144 1048576 4194304
+net.ipv4.tcp_congestion_control = bbr
+net.core.default_qdisc = fq
+net.ipv4.ip_local_port_range = 10000 65535
+net.ipv4.ping_group_range = 0 2147483647
 EOF
 sysctl -p >/dev/null 2>&1
 sysctl --system >/dev/null 2>&1
