6f8490b728ce1243ed29d1c716a08b1ac5912be8
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2023-09-09 00:54:30 +0800

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 安装软件包
-apt install -y sudo bash-completion vim curl wget ntp net-tools zram-tools dnsutils vnstat fail2ban iperf3 qemu-guest-agent
+apt install -y sudo bash-completion vim curl wget ntp net-tools zram-tools dnsutils vnstat iperf3 qemu-guest-agent
 
 #开启 zram
 echo -e "ALGO=zstd\nPERCENT=100" > /etc/default/zramswap
