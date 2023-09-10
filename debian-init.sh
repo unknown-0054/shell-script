4c09c609e30c91e6cdded475ad1a5f91e536616d
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2023-09-10 21:28:47 +0800

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -4,53 +4,6 @@
 apt install -y sudo bash-completion vim curl wget ntp net-tools zram-tools fail2ban dnsutils vnstat iperf3 qemu-guest-agent
 
 #开启 zram
-echo -e "ALGO=zstd\nPERCENT=100" > /etc/default/zramswap
-service zramswap reload
-
-# 去除布告栏信息
-echo '' >/etc/motd
-echo '' >/etc/issue
-
-# Fail2ban
-cat <<EOF > /etc/fail2ban/jail.d/defaults-debian.conf
-[sshd]
-enabled = true
-backend=systemd
-EOF
-systemctl restart fail2ban
-
-# 屏蔽 docker.io
-cat <<EOF > /etc/apt/preferences.d/docker
-Package: docker docker.io docker-compose 
-Pin: release *
-Pin-Priority: -1
-EOF
-
-# hook make_resolv_conf 函数(避免dhclient对/etc/resolv.conf的修改)
-cat <<EOF >/etc/dhcp/dhclient-enter-hooks.d/nodnsupdate
-#!/bin/sh
-make_resolv_conf(){
-    :
-}
-EOF
-chmod +x /etc/dhcp/dhclient-enter-hooks.d/nodnsupdate
-
-# vim 禁用鼠标
-echo "set mouse-=a" >~/.vimrc
-
-# 添加别名
-if ! grep -q "alias dc" ~/.bashrc; then
-    echo "alias dc='docker compose'" >> ~/.bashrc
-fi
-source ~/.bashrc
-
-rm -rf /etc/sysctl.d/*
-#!/bin/bash
-
-# 安装软件包
-apt install -y sudo bash-completion vim curl wget ntp net-tools zram-tools fail2ban dnsutils vnstat iperf3 qemu-guest-agent
-
-#开启 zram
 echo -e "ALGO=zstd\nPERCENT=100" >/etc/default/zramswap
 service zramswap reload
 
