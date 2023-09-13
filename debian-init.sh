ee946394adf5c6f19086e140ebbd3d249b318c2d
unknown0054 <93303847+unknown-0054@users.noreply.github.com>
2023-09-14 00:53:41 +0800

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -37,6 +37,83 @@ else
     rm -rf /opt/containerd
 fi
 
+# tcp
+cat <<EOF >/etc/sysctl.d/99-sysctl.conf
+fs.file-max=1000000
+fs.inotify.max_user_instances=8192
+net.ipv4.tcp_max_syn_backlog=8192
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.ipv4.ip_forward=1
+net.core.somaxconn=3276800
+net.core.optmem_max=81920
+net.core.wmem_default=131072
+net.core.rmem_default=131072
+net.core.wmem_max=16777216
+net.core.rmem_max=16777216
+net.ipv4.tcp_mem=786432 1048576 3145728 
+net.ipv4.tcp_wmem=4096 131072 16777216
+net.ipv4.tcp_rmem=4096 131072 16777216
+net.ipv4.ip_local_port_range = 50000 65535
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+   if [[ ${totalMem//.*/} -lt 4 ]]; then    #<4GB 1G_3G_8G
+      sed -i "s#.*net.ipv4.tcp_mem=.*#net.ipv4.tcp_mem=262144 786432 2097152#g" /etc/sysctl.conf
+      tcpmeminfo="应用4GB内存TCP方案"
+   elif [[ ${totalMem//.*/} -ge 4 && ${totalMem//.*/} -lt 7 ]]; then    #6GB 2G_4G_8G
+      sed -i "s#.*net.ipv4.tcp_mem=.*#net.ipv4.tcp_mem=524288 1048576 2097152#g" /etc/sysctl.conf
+      tcpmeminfo="应用6GB内存TCP方案"
+   elif [[ ${totalMem//.*/} -ge 7 && ${totalMem//.*/} -lt 11 ]]; then    #8GB 3G_4G_12G
+      sed -i "s#.*net.ipv4.tcp_mem=.*#net.ipv4.tcp_mem=786432 1048576 3145728#g" /etc/sysctl.conf
+      tcpmeminfo="应用8GB内存TCP方案"
+   elif [[ ${totalMem//.*/} -ge 11 && ${totalMem//.*/} -lt 15 ]]; then    #12GB 4G_6G_12G
+      sed -i "s#.*net.ipv4.tcp_mem=.*#net.ipv4.tcp_mem=1048576 1572864 3145728#g" /etc/sysctl.conf
+      tcpmeminfo="应用12GB内存TCP方案"
+   elif [[ ${totalMem//.*/} -ge 15 ]]; then     #>16GB 4G_8G_12G
+      sed -i "s#.*net.ipv4.tcp_mem=.*#net.ipv4.tcp_mem=1048576 2097152 3145728#g" /etc/sysctl.conf
+      tcpmeminfo="应用16GB内存TCP方案"
+   fi
+   
+   echo "1000000" > /proc/sys/fs/file-max
+   sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
+   echo "session required pam_limits.so" >>/etc/pam.d/common-session
+   sed -i '/ulimit -SHn/d' /etc/profile
+   echo "ulimit -SHn 1000000" >>/etc/profile
+   ulimit -SHn 1000000 && ulimit -c unlimited
+   echo "*     soft   nofile    1048576
+*     hard   nofile    1048576
+*     soft   nproc     1048576
+*     hard   nproc     1048576
+*     soft   core      1048576
+*     hard   core      1048576
+*     hard   memlock   unlimited
+*     soft   memlock   unlimited">/etc/security/limits.conf
+
+   sed -i '/DefaultTimeoutStartSec/d' /etc/systemd/system.conf
+   sed -i '/DefaultTimeoutStopSec/d' /etc/systemd/system.conf
+   sed -i '/DefaultRestartSec/d' /etc/systemd/system.conf
+   sed -i '/DefaultLimitCORE/d' /etc/systemd/system.conf
+   sed -i '/DefaultLimitNOFILE/d' /etc/systemd/system.conf
+   sed -i '/DefaultLimitNPROC/d' /etc/systemd/system.conf
+   echo "#DefaultTimeoutStartSec=90s
+DefaultTimeoutStopSec=30s
+#DefaultRestartSec=100ms
+DefaultLimitCORE=infinity
+DefaultLimitNOFILE=20480000
+DefaultLimitNPROC=20480000">>/etc/systemd/system.conf
+   systemctl daemon-reload
+   systemctl daemon-reexec
+
 # 添加别名
 if ! grep -q "alias dc" ~/.bashrc; then
   echo "alias dc='docker compose'" >>~/.bashrc
